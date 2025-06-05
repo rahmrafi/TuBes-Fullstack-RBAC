6fcb6ad02ad5a30f171d877e65f2a57bcf6d7a4e
rahmrafi <rafiansyah2604@gmail.com>
2025-06-05 20:31:33 +0700

    Tugas Besar Sistem Basisdata

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/database');
+
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 2526;
+
+
+app.use(cors());
+app.use(express.json());
+
+db.authenticate()
+    .then(() => console.log('Terkoneksi ke database!'))
+    .catch(err => console.log('Tidak dapat terhubung ke database :' + err));
+
+db.sync({ force: false})
+    .then(() => console.log('Database sudah disinkronisasi!'))
+    .catch(err => console.error('Gagal menyinkronisasi database :', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API sedang berjalan...');
+});
+
+app.listen(PORT, () => {
+    console.log('Server berjalan pada port ${PORT}');
+});
